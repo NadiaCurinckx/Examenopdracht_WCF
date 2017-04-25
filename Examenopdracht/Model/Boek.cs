b899ac03fa50c66d76c9aa7679bb0e504ab188f4
Nadia C <Nadia C@DESKTOP-CGGQBLM>
2017-04-25 18:33:35 +0200

    Add model

---
+++ Examenopdracht/Model/Boek.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Model
+{
+    class Boek
+    {
+        public Int32 Id { get; set; }
+        public String Titel { get; set; }
+        public String Auteur { get; set; }
+        public Int32 AantalPaginas { get; set; }
+        public virtual ICollection<Genre> Genres { get; set; }
+    }
+}
---
+++ Examenopdracht/Model/Genre.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Model
+{
+    class Genre
+    {
+        public Int32 Id { get; set; }
+        public String Omschrijving { get; set; }
+        public virtual ICollection<Boek> Boeken { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Omschrijving}";
+        }
+    }
+}
