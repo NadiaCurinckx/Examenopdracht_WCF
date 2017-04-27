b294e062e5a8ee8e01b5de30ea86bf6b1a4686e4
Nadia C <Nadia C@DESKTOP-CGGQBLM>
2017-04-27 20:45:30 +0200

    UI boek toevoegen en verwijderen half en half

--- Examenopdracht/WPF/MainWindow.xaml.cs
+++ Examenopdracht/WPF/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
 ﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.ServiceModel;
 using System.Threading.Tasks;
 using System.Windows;
@@ -28,15 +31,14 @@ namespace WPF
 
         }
 
-        //private async Task LijstenVernieuwen()
-        //{
-            
-        //}
+
 
 
         private void btnBoekToevoegen_Click(object sender, RoutedEventArgs e)
         {
-
+            BewaarBoek();
+            ToonBoeken();
+            MaakVeldenLeeg();
         }
 
         private void btnBoekBewerken_Click(object sender, RoutedEventArgs e)
@@ -46,11 +48,112 @@ namespace WPF
 
         private void btnBoekVerwijderen_Click(object sender, RoutedEventArgs e)
         {
-
+            VerwijderBoek();
+            ToonBoeken();
+            MaakVeldenLeeg();
         }
 
         private void lsbBoeken_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
         {
+
+        }
+
+
+
+
+        public bool IsGeldigBoek()
+        {
+            if (string.IsNullOrEmpty(txtTitel.Text))
+            {
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(txtAuteur.Text))
+            {
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(txtAantalPaginas.Text))
+            {
+                return false;
+            }
+
+            try
+            {
+                Convert.ToInt32(txtAantalPaginas.Text);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public async void BewaarBoek()
+        {
+            if (IsGeldigBoek())
+            {
+                Boek boek = new Boek()
+                {
+                    Titel = txtTitel.Text,
+                    Auteur = txtAuteur.Text,
+                    AantalPaginas = Convert.ToInt32(txtAantalPaginas.Text),
+                    Genres = new List<Genre>()
+                };
+
+
+                //using (var database = new Database())
+                //{
+                //    foreach (var genre in NeemGeselecteerdeGenres())
+                //    {
+                //        boek.Genres.Add(database.Genres.SingleOrDefault(x => x.Id == genre.Id));
+                //    }
+                //}
+
+                await _boekLogica.BewaarBoek(boek);
+
+            }
+
+            else
+            {
+                MessageBox.Show("Controleer of je alle velden correct hebt ingevuld.", "Fout tijdens bewaren van het boek", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        public List<Genre> NeemGeselecteerdeGenres()
+        {
+            return lsbGenre.SelectedItems.Cast<Genre>().ToList();
+        }
+
+        public void ToonBoeken()
+        {
+            lsbBoeken.Items.Clear();
+
+            Task<List<Boek>> boekenlijst = _boekLogica.NeemAlleBoeken();
+
+            foreach (var boek in boekenlijst)
+            {
+                lsbBoeken.Items.Add(boek);
+            }
+        }
+
+        public void MaakVeldenLeeg()
+        {
+            txtTitel.Text = "";
+            txtAuteur.Text = "";
+            txtAantalPaginas.Text = "";
+            lsbGenre.SelectedItems.Clear();
+        }
+
+        public void VerwijderBoek()
+        {
+            Boek geselecteerdBoek = (Boek)lsbBoeken.SelectedItem;
+
+            if (geselecteerdBoek != null)
+            {
+                _boekLogica.VerwijderBoek(geselecteerdBoek.Id);
+            }
 
         }
     }
